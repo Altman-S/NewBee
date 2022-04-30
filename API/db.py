7bcd6097441103c8b3169d095c8648a8faecac0a
Pi <altmanpp@gmail.com>
2022-04-30 19:55:53 +0100

    Hide database uri

--- API/db.py
+++ API/db.py
@@ -6,7 +6,8 @@ db = None
 
 
 def get_db():
-    uri = "mongodb+srv://m001-student:789654123@sandbox.xzdvv.mongodb.net/test"
+    # add database uri
+    uri = "#######"
     client = MongoClient(uri)
     db = client.sample_movies
     return db
